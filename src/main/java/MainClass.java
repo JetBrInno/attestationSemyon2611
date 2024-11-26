cea5ff6c1add4b229edafe0fcae8caeb0066cd08
SamSt <77111687+Sammykennylol@users.noreply.github.com>
2024-11-26 21:55:06 +0400

    added new line again

--- src/main/java/MainClass.java
+++ src/main/java/MainClass.java
@@ -3,5 +3,6 @@ public class MainClass {
     public static void main(String[] args) {
         System.out.println("Мой проект!");
         System.out.println("Новая строка");
+        System.out.println("Еще строкааааа");
     }
 }
